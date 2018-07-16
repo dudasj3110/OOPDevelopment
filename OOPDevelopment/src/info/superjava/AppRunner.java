d23a548e2134deb6467b4bd7be70e15c442555b6
Direk <Direk@DESKTOP-ICFFKV8>
2018-07-16 23:12:38 +0300

    fix triangle

--- OOPDevelopment/src/info/superjava/AppRunner.java
+++ OOPDevelopment/src/info/superjava/AppRunner.java
@@ -1,5 +1,14 @@
 package info.superjava;
 
+import java.util.logging.Logger;
+
 public class AppRunner {
 
+	private static Logger logger = Logger.getLogger(AppRunner.class.getName());
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
 }
