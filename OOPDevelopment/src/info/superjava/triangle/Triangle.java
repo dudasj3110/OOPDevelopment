93d77776a25f682b1c9b6bfc2e70d6ea550a2fd8
Direk <Direk@DESKTOP-ICFFKV8>
2018-07-16 15:18:30 +0300

    homework triangle

--- OOPDevelopment/src/info/superjava/triangle/Triangle.java
+++ OOPDevelopment/src/info/superjava/triangle/Triangle.java
@@ -1,13 +1,31 @@
 package info.superjava.triangle;
 
+import java.awt.Point;
+
 import info.superjava.AbstractShape;
 
 public class Triangle extends AbstractShape {
 
-	private int side ;
 	
-	
-	
+	private  Point a;
+	private  Point b;
+	private  Point c;
+ 
+	public Triangle(Point a, Point b, Point c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+ 
+	private double area = -1;
+ 
+	public double area() {
+		if(area == -1) {
+			double ab = a.distanceTo(b);
+			double bc = b.distanceTo(c);
+			double ac = a.distanceTo(c);
+			double p = (ab + bc + ac) / 2;
+			area = Math.sqrt(p * (p-ab) * (p-bc) * (p-ac));
 	
 	@Override
 	public int getArea() {
@@ -15,8 +33,4 @@ public class Triangle extends AbstractShape {
 		return 0;
 	}
 
-	
-	
-	
-	
 }
