9633d9fce0be9f10da5f3c7ca1ba783106a7980e
Direk <Direk@DESKTOP-ICFFKV8>
2018-07-16 23:10:34 +0300

    fix triangle

--- OOPDevelopment/src/info/superjava/triangle/Triangle.java
+++ OOPDevelopment/src/info/superjava/triangle/Triangle.java
@@ -1,32 +1,31 @@
 package info.superjava.triangle;
 
-import java.awt.Point;
-
 import info.superjava.AbstractShape;
 
 public class Triangle extends AbstractShape {
 
+	private int base;
 	
-	private  Point a;
-	private  Point b;
-	private  Point c;
- 
-	public Triangle(Point a, Point b, Point c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
+	private int height;
+	
+	
+	public int getBase() {
+		return base;
 	}
- 
-	private double area = -1;
- 
-	public double area() {
-		if(area == -1) {
-			double ab = a.distanceTo(b);
-			double bc = b.distanceTo(c);
-			double ac = a.distanceTo(c);
-			double p = (ab + bc + ac) / 2;
-			area = Math.sqrt(p * (p-ab) * (p-bc) * (p-ac));
-	
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	@Override
 	public int getArea() {
 		// TODO Auto-generated method stub
